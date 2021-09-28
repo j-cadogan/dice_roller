aa2787aff5ac1f3dca8d2ff91af62938888887dd
Jaclyn Cadogan <jcadogan@email.arizona.edu>
2021-09-28 16:46:30 -0700

    cat.py final

---
+++ assignments/04_cat/cat.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Author : jcadogan <jcadogan@localhost>
+Date   : 2021-09-28
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Python cat',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        help='Input file(s)',
+                        nargs='+',
+                        default=[sys.stdin])
+
+    parser.add_argument('-n',
+                        '--number',
+                        help='Number the lines',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    for fh in args.file:
+        num_lines = 0
+        for line in fh:
+            num_lines += 1
+            if args.number:
+                print(f'{num_lines:6}' + '\t' + line.rstrip())
+            else:
+                print(line.rstrip())
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
