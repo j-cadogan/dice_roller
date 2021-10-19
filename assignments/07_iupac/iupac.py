1395ec304a9d24f78b5995c46762473e966c5e35
Jaclyn Cadogan <jcadogan@email.arizona.edu>
2021-10-19 15:42:54 -0700

    Passes up through test_7. Got rid of for loops and if statements and used maketrans() and translate() methods instead.

--- assignments/07_iupac/iupac.py
+++ assignments/07_iupac/iupac.py
@@ -37,23 +37,41 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    code = args.SEQ
+    code_input = args.SEQ
 
+    '''
     code_base = {'A':'A', 'C':'C', 'G':'G', 'T':'T', 'U':'U', 'R':'AG',
                  'Y':'CT', 'S':'GC', 'W':'AT', 'K':'GT', 'M':'AC',
                  'B':'CGT', 'D':'AGT', 'H':'ACT', 'V':'ACG', 'N':'ACGT'}
-
-    for sequence in code:
+    '''
+    code_base = {'A':'A', 'C':'C', 'G':'G', 'T':'T', 'U':'U', 'R':'[AG]',
+                 'Y':'[CT]', 'S':'[GC]', 'W':'[AT]', 'K':'[GT]', 'M':'[AC]',
+                 'B':'[CGT]', 'D':'[AGT]', 'H':'[ACT]', 'V':'[ACG]', 'N':'[ACGT]'}
+    '''
+    for sequence in code_input:
         base_str = ''
         #print(sequence)
         for letter in sequence:
-            #print(letter, code_base[letter])
+            print(letter, code_base[letter])
             if len(code_base[letter]) > 1:
                 base_str += base_str.join('[' + code_base[letter] + ']')
+                print('[' + code_base[letter] + ']')
             else:
                 base_str += base_str.join(code_base[letter])
+            print(base_str)
         print(sequence, base_str)
-
+    
+    for sequence in code_input:
+        base_str = ''
+        for letter in sequence:
+            tmp = []
+            if len(code_base[letter]) > 1:
+                tmp += ['[', code_base[letter], ']']
+                print(tmp.join())
+    '''
+    for sequence in code_input:
+        translation = sequence.maketrans(code_base)
+        print(sequence, sequence.translate(translation))
 
 # --------------------------------------------------
 if __name__ == '__main__':
