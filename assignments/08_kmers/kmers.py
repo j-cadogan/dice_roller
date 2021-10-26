3122e87acca83c48930f778007e1070aa1da079a
Jaclyn Cadogan <jcadogan@email.arizona.edu>
2021-10-26 15:40:52 -0700

    Passes all tests.

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -2,7 +2,8 @@
 """
 Author : jcadogan <jcadogan@localhost>
 Date   : 2021-10-26
-Purpose: Rock the Casbah
+Purpose: Find common k-mers between two files, with customizable
+         k-mer length.
 """
 
 import argparse
@@ -46,7 +47,36 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    
+    text1 = args.file1.read().split()
+    text2 = args.file2.read().split()
+
+    kmers1 = kmers_dict(text1, args.kmer)
+    kmers2 = kmers_dict(text2, args.kmer)
+
+    common = sorted(list(set(kmers1) & set(kmers2)))
+    for i in common:
+        print(f'{i:10} {kmers1[i]:5} {kmers2[i]:5}')
+
+
+# --------------------------------------------------
+def find_kmers(seq, k):
+    """Find k-mers in string"""
+    n = len(seq) - k + 1
+    return [] if n < 1 else [seq[i:i + k] for i in range(n)]
+
+
+# --------------------------------------------------
+def kmers_dict(text, k):
+    """Add k-mers and their frequency to dictionary."""
+    kmers = {}
+    for seq in text:
+        for kmer in find_kmers(seq, k):
+            if kmer not in kmers:
+                kmers.update({kmer: 1})
+            else:
+                kmers[kmer] += 1
+    return kmers
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
